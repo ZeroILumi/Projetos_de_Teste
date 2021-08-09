755e5a9b980b1cdbecdfc3f5f1de25c1f89fe755
ZeroILumi <zeroilumi666@gmail.com>
2021-08-09 11:15:15 -0300

    Adição de diversos codigos em python

--- Meus Projetos de Teste/Testes_Complexos/Projetos Python/meu_app.py
+++ Meus Projetos de Teste/Testes_Complexos/Projetos Python/meu_app.py
@@ -1,5 +1,6 @@
-print('Meu Primeiro Programa em Pythom')
-a = 2
-b = 4
-soma = a + b
-print(soma)
+if __name__ == '__main__':
+    print('Meu Primeiro Programa em Pythom')
+    a = 2
+    b = 4
+    soma = a + b
+    print(soma)
